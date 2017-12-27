b4933b1d1d88b4463b121114892ab4ab006ad222
Hieupv <hieupv@codersvn.com>
2017-12-27 10:27:04 +0700

    fix console error

--- src/Providers/RestApiProvider.php
+++ src/Providers/RestApiProvider.php
@@ -10,24 +10,25 @@ class RestApiProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(Router::class, function ($app) {
-            return new Router;
-        });
-
-        $route_file = $this->app->appPath() . '/routes/api.php';
-        if (file_exists($route_file)) {
-            $api = require_once $route_file;
-
-            $routes = $api->routes;
-
-            add_action('rest_api_init', function () use ($routes) {
-                foreach ($routes as $route) {
-                    register_rest_route($route['namespace'], $route['uri'], $route['args']);
-                }
+        if (!$this->app->runningInConsole()) {
+            $this->app->singleton(Router::class, function ($app) {
+                return new Router;
             });
 
-        }
+            $route_file = $this->app->appPath() . '/routes/api.php';
+            if (file_exists($route_file)) {
+                $api = require_once $route_file;
 
+                $routes = $api->routes;
+
+                add_action('rest_api_init', function () use ($routes) {
+                    foreach ($routes as $route) {
+                        register_rest_route($route['namespace'], $route['uri'], $route['args']);
+                    }
+                });
+
+            }
+        }
     }
 
     public function registerCommand()
